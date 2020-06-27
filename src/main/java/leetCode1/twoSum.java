0c570a4815aa1957a94ed13394258953baabb0c9
bspp1314 <linyuanpeng1314@gmail.com>
2020-06-27 16:33:58 +0800

    feat:two sum and max Area

---
+++ src/main/java/leetCode1/twoSum.java
@@ -0,0 +1,37 @@
+package leetCode1;
+
+import java.util.HashMap;
+
+public class twoSum {
+
+    public static int[] twoSum(int[] nums,int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> targetMap = new HashMap<Integer,Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (targetMap.containsKey(target-nums[i])){
+                result[0] = targetMap.get(target-nums[i]);
+                result[1] = i;
+                return result;
+            }
+
+            targetMap.put(nums[i],i);
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int target = 5;
+
+        int[] res = twoSum(nums,target);
+
+        for (int i = 0; i < res.length ; i++) {
+            System.out.printf(" %d ",res[i]);
+        }
+        System.out.println();
+
+
+    }
+}
---
+++ src/main/java/leetCode11/maxArea.java
@@ -0,0 +1,29 @@
+package leetCode11;
+
+public class maxArea {
+    //双指针法
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+
+        int maxArea = 0;
+
+        while (i < j) {
+            int newArea = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+            maxArea = maxArea > newArea ? maxArea : newArea;
+
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return maxArea;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
